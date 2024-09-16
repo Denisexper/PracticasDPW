2aab66b810fe3be3f2672c53c4771ae49f1a4250
Denisexper <clasic.exper2023@gmail.com>
2024-09-16 10:14:10 -0600

    practica 6

---
+++ practia-6dwa/declaraciones.js
@@ -0,0 +1,11 @@
+//declaraciones var (puede ser declarada y reasignada)
+
+var nombre = 'Alexander Canales'
+console.log(nombre)
+console.log("Hello my name is  " + nombre)
+
+//declaraciones let
+let edad = 24;
+console.log(edad);
+
+//
